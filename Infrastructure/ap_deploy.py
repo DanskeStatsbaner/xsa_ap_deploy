62aa486af41f4897358e257af7c82598bdad559e
milimat0810 <milimat@dsb.dk>
2021-10-13 14:04:34 +0200

    Initial

--- Infrastructure/ap_deploy.py
+++ Infrastructure/ap_deploy.py
@@ -75,7 +75,7 @@ with open('../../xs-security.json') as file:
         xs_security['scopes'][index]['name'] = f'$XSAPPNAME.SHIP.{hana_environment_upper}.{project_name}_{scope["name"]}'
 
     for index, role in enumerate(xs_security['role-templates']):
-        xs_security['role-templates'][index]['name'] = f'$XSAPPNAME.{hana_environment_upper}.{project_name}_{role["name"]}'
+        xs_security['role-templates'][index]['name'] = f'{hana_environment_upper}.{project_name}_{role["name"]}'
         xs_security['role-templates'][index]['scope-references'] = [f'$XSAPPNAME.SHIP.{hana_environment_upper}.{project_name}_{scope}' for scope in role['scope-references']]
     
     xs_security = json.dumps(xs_security, indent=2)

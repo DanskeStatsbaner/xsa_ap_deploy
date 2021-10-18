8f681d40ebe36cc9a8f5b36152cf4deddd852d9a
milimat0810 <milimat@dsb.dk>
2021-10-18 17:13:10 +0200

    Refactor

--- Infrastructure/ap_deploy.py
+++ Infrastructure/ap_deploy.py
@@ -12,109 +12,7 @@ xsa_space = get_octopusvariable("dataART.XSASpace")
 xsa_pass = sys.argv[1]
 
 hana_environment = get_octopusvariable("dataART.Database").lower()
-
-with open('../../manifest.yml') as manifest:
-    manifest_yaml = manifest.read()
-    
-manifest_dict = yaml.safe_load(manifest_yaml)
-
-project_type = manifest_dict['type']
-
-if project_type != 'python':
-    failstep('The pipeline only supports Python XSA applications.')
-
-services = manifest_dict['services']
-
-host = project_name.lower().replace('_', '-')
-app_router = f'{project_name}-sso'
-app_router_host = app_router.lower().replace('_', '-')
-uaa_service = f'{project_name}-uaa'
-url = lambda subdomain: f"https://{subdomain}.xsabi{hana_environment}.dsb.dk:30033"
-
-services += [uaa_service]
-
-manifest_dict = {
-    'applications': [
-        {
-            'name': project_name,
-            'host': host,
-            'path': './app/',
-            'command': 'python api.py',
-            'services': services
-        },
-        {
-            'name': app_router,
-            'host': app_router_host,
-            'path': './app-router/',
-            'env': {
-                'destinations': json.dumps([{"name": project_name, "url": url(host), "forwardAuthToken": True}])
-            },
-            'services': [
-                uaa_service
-            ]
-        }
-    ]
-}
-
-manifest_yaml = yaml.dump(manifest_dict)
-
-with open('../../manifest.yml', 'w') as file:
-    file.write(manifest_yaml)
-
-with open('../../app/manifest', 'w') as file:
-    file.write(manifest_yaml)
-
-with open('../../app/api.py') as api:
-    api_content = api.read()
-    api_content = api_content.replace('OCTOPUS_APP_ROUTER_URL', url(app_router_host))
-
-with open('../../app/api.py', 'w') as file:
-    file.write(api_content)
-
-with open('../../app-router/xs-app.json') as file:
-    xs_app = json.loads(file.read())
-    xs_app['welcomeFile'] = f"/{host}"
-    xs_app['routes'][0]['source'] = f"/{host}(.*)"
-    xs_app['routes'][0]['destination'] = project_name
-    xs_app = json.dumps(xs_app, indent=2)
-
-with open('../../app-router/xs-app.json', 'w') as file:
-    file.write(xs_app)
-
-with open('../../app-router/package.json') as file:
-    package = json.loads(file.read())
-    package['name'] = f"{host}-approuter"
-    package = json.dumps(package, indent=2)
-
-with open('../../app-router/package.json', 'w') as file:
-    file.write(package)
-
 hana_environment_upper = hana_environment.upper()
-
-with open('../../xs-security.json') as file:
-    xs_security = json.loads(file.read())
-    xs_security['xsappname'] = project_name
-    
-    for index, scope in enumerate(xs_security['scopes']):
-        xs_security['scopes'][index]['name'] = f'$XSAPPNAME.{scope["name"]}'
-        
-    scopes = [scope['name'] for scope in xs_security['scopes']]
-    role_collections = []
-    mappings = []
-
-    for index, role in enumerate(xs_security['role-templates']):
-        role_collection = f'{project_name}_{role["name"]}'
-        role_collections += [role_collection]
-        mappings += [[role_collection, f'SHIP.{hana_environment_upper}.{scope}'] for scope in role['scope-references']]
-        xs_security['role-templates'][index]['name'] = f'{project_name}_{role["name"]}'
-        xs_security['role-templates'][index]['scope-references'] = [f'$XSAPPNAME.{scope}' for scope in role['scope-references']]
-        
-    roles = [role['name'] for role in xs_security['role-templates']]
-
-    xs_security = json.dumps(xs_security, indent=2)
-
-with open('../../xs-security.json', 'w') as file:
-    file.write(xs_security)
 
 def check_output(cmd, show_output=True, show_cmd=True, docker=True):
     if docker:
@@ -131,54 +29,157 @@ def check_output(cmd, show_output=True, show_cmd=True, docker=True):
             print(line)
     return output
 
-def delete_manifest():
-    if os.path.exists('app/manifest'):
-        os.remove('app/manifest')
+if __name__ == '__main__':
 
-check_output(f'xs login -u {xsa_user} -p {xsa_pass} -a {xsa_url} -o orgname -s {xsa_space}', show_cmd=False)
+    with open('../../manifest.yml') as manifest:
+        manifest_yaml = manifest.read()
+        
+    manifest_dict = yaml.safe_load(manifest_yaml)
 
-manifest_path = check_output(f'cd /data && find . -name manifest.yml', show_output=False, show_cmd=False)
-deploy_path = os.path.dirname(manifest_path).replace('./', '')
+    project_type = manifest_dict['type']
 
-output = check_output(f'xs service {uaa_service}', show_output=True)
-xs_security = '-c xs-security.json' if os.path.exists('../../xs-security.json') else ''
+    if project_type != 'python':
+        failstep('The pipeline only supports Python XSA applications.')
 
-if 'failed' in output:
-    failstep(f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
-elif not 'succeeded' in output:
-    output = check_output(f'cd /data/{deploy_path} && xs create-service xsuaa default {uaa_service} {xs_security}', show_output=True)
-    if 'FAILED' in output:
-        failstep(f'Creation of the service "{uaa_service}" failed' + '\n'.join([line for line in output.split('\n') if 'FAILED' in line]))
-    else:   
-        printhighlight(f'The service "{uaa_service}" was succesfully created')
-else:
-    output = check_output(f'cd /data/{deploy_path} && xs update-service {uaa_service} {xs_security}', show_output=True)
+    services = manifest_dict['services']
+
+    host = project_name.lower().replace('_', '-')
+    app_router = f'{project_name}-sso'
+    app_router_host = app_router.lower().replace('_', '-')
+    uaa_service = f'{project_name}-uaa'
+    url = lambda subdomain: f"https://{subdomain}.xsabi{hana_environment}.dsb.dk:30033"
+
+    services += [uaa_service]
+
+    manifest_dict = {
+        'applications': [
+            {
+                'name': project_name,
+                'host': host,
+                'path': './app/',
+                'command': 'python api.py',
+                'services': services
+            },
+            {
+                'name': app_router,
+                'host': app_router_host,
+                'path': './app-router/',
+                'env': {
+                    'destinations': json.dumps([{"name": project_name, "url": url(host), "forwardAuthToken": True}])
+                },
+                'services': [
+                    uaa_service
+                ]
+            }
+        ]
+    }
+
+    manifest_yaml = yaml.dump(manifest_dict)
+
+    with open('../../manifest.yml', 'w') as file:
+        file.write(manifest_yaml)
+
+    with open('../../app/manifest', 'w') as file:
+        file.write(manifest_yaml)
+
+    with open('../../app/api.py') as api:
+        api_content = api.read()
+        api_content = api_content.replace('OCTOPUS_APP_ROUTER_URL', url(app_router_host))
+
+    with open('../../app/api.py', 'w') as file:
+        file.write(api_content)
+
+    with open('../../app-router/xs-app.json') as file:
+        xs_app = json.loads(file.read())
+        xs_app['welcomeFile'] = f"/{host}"
+        xs_app['routes'][0]['source'] = f"/{host}(.*)"
+        xs_app['routes'][0]['destination'] = project_name
+        xs_app = json.dumps(xs_app, indent=2)
+
+    with open('../../app-router/xs-app.json', 'w') as file:
+        file.write(xs_app)
+
+    with open('../../app-router/package.json') as file:
+        package = json.loads(file.read())
+        package['name'] = f"{host}-approuter"
+        package = json.dumps(package, indent=2)
+
+    with open('../../app-router/package.json', 'w') as file:
+        file.write(package)
+
+    with open('../../xs-security.json') as file:
+        xs_security = json.loads(file.read())
+        xs_security['xsappname'] = project_name
+        
+        for index, scope in enumerate(xs_security['scopes']):
+            xs_security['scopes'][index]['name'] = f'$XSAPPNAME.{scope["name"]}'
+            
+        scopes = [scope['name'] for scope in xs_security['scopes']]
+        role_collections = []
+        mappings = []
+
+        for index, role in enumerate(xs_security['role-templates']):
+            role_collection = f'{project_name}_{role["name"]}'
+            role_collections += [role_collection]
+            mappings += [[role_collection, f'SHIP.{hana_environment_upper}.{scope}'] for scope in role['scope-references']]
+            xs_security['role-templates'][index]['name'] = f'{project_name}_{role["name"]}'
+            xs_security['role-templates'][index]['scope-references'] = [f'$XSAPPNAME.{scope}' for scope in role['scope-references']]
+            
+        roles = [role['name'] for role in xs_security['role-templates']]
+
+        xs_security = json.dumps(xs_security, indent=2)
+
+    with open('../../xs-security.json', 'w') as file:
+        file.write(xs_security)
+
+    def delete_manifest():
+        if os.path.exists('app/manifest'):
+            os.remove('app/manifest')
+
+    check_output(f'xs login -u {xsa_user} -p {xsa_pass} -a {xsa_url} -o orgname -s {xsa_space}', show_cmd=False)
+
+    manifest_path = check_output(f'cd /data && find . -name manifest.yml', show_output=False, show_cmd=False)
+    deploy_path = os.path.dirname(manifest_path).replace('./', '')
+
+    output = check_output(f'xs service {uaa_service}', show_output=True)
+    xs_security = '-c xs-security.json' if os.path.exists('../../xs-security.json') else ''
 
     if 'failed' in output:
         failstep(f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
-        
-output = check_output(f'cd /data/{deploy_path} && xs push {app_router}')
+    elif not 'succeeded' in output:
+        output = check_output(f'cd /data/{deploy_path} && xs create-service xsuaa default {uaa_service} {xs_security}', show_output=True)
+        if 'FAILED' in output:
+            failstep(f'Creation of the service "{uaa_service}" failed' + '\n'.join([line for line in output.split('\n') if 'FAILED' in line]))
+        else:   
+            printhighlight(f'The service "{uaa_service}" was succesfully created')
+    else:
+        output = check_output(f'cd /data/{deploy_path} && xs update-service {uaa_service} {xs_security}', show_output=True)
 
-app_url = [line.split(':', 1)[1].strip() for line in output.split('\n') if 'urls' in line][0] + '/' + host
+        if 'failed' in output:
+            failstep(f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
+            
+    output = check_output(f'cd /data/{deploy_path} && xs push {app_router}')
 
-output = check_output(f'cd /data/{deploy_path} && xs push {project_name}')
+    app_url = [line.split(':', 1)[1].strip() for line in output.split('\n') if 'urls' in line][0] + '/' + host
 
-is_running = output.rfind('RUNNING') > output.rfind('CRASHED')
+    output = check_output(f'cd /data/{deploy_path} && xs push {project_name}')
 
-if is_running:
-    printhighlight(f'The application is running: {app_url}')
-else:
-    failstep('The application crashed')
+    is_running = output.rfind('RUNNING') > output.rfind('CRASHED')
+
+    if is_running:
+        printhighlight(f'The application is running: {app_url}')
+    else:
+        failstep('The application crashed')
 
 
 
-for role_collection in role_collections:
-    check_output(f'xs delete-role-collection {role_collection} -f -u {xsa_user} -p {xsa_pass}', show_cmd=False)
-    check_output(f'xs create-role-collection {role_collection} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
-    check_output(f'xs update-role-collection {role_collection} --add-role {role_collection} -s {xsa_space} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
+    for role_collection in role_collections:
+        check_output(f'xs delete-role-collection {role_collection} -f -u {xsa_user} -p {xsa_pass}', show_cmd=False)
+        check_output(f'xs create-role-collection {role_collection} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
+        check_output(f'xs update-role-collection {role_collection} --add-role {role_collection} -s {xsa_space} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
 
-check_output(f'docker cp cockpit.py {container_name}:/tmp/cockpit.py', docker=False)
+    check_output(f'docker cp cockpit.py {container_name}:/tmp/cockpit.py', docker=False)
 
-mappings = json.dumps(mappings).replace('"', '\\"')
+    mappings = json.dumps(mappings).replace('"', '\\"')
 
-check_output(f"python3 /tmp/cockpit.py -u {xsa_user} -p {xsa_pass} -a {xsa_url} -m '{mappings}'", show_cmd=False)
+    check_output(f"python3 /tmp/cockpit.py -u {xsa_user} -p {xsa_pass} -a {xsa_url} -m '{mappings}'", show_cmd=False)

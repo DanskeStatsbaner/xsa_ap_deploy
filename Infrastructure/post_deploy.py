ef7a6a013942a10e4eb8d67129ec92535eb56252
NIHO1009 <nichol@dsb.dk>
2021-10-26 10:13:01 +0200

    hej

--- Infrastructure/post_deploy.py
+++ Infrastructure/post_deploy.py
@@ -1,4 +1,5 @@
 import subprocess, json, sys, os
+from hdbcli import dbapi
 
 environment = get_octopusvariable("Octopus.Environment.Name").lower()
 
@@ -8,11 +9,14 @@ container_name = f"dataArt.{project_name}.{release_number}.{environment}"
 
 xsa_url = get_octopusvariable("dataART.XSAUrl")
 xsa_user = get_octopusvariable("dataART.XSAUser")
+xsa_keyuser = get_octopusvariable("dataART.XSAKeyUser")
 xsa_space = get_octopusvariable("dataART.XSASpace")
 xsa_pass = sys.argv[1]
 
 hana_environment = get_octopusvariable("dataART.Database").lower()
 hana_environment_upper = hana_environment.upper()
+hana_host = get_octopusvariable("dataART.Host").split('.')[0]
+hana_port = 30015
 
 def check_output(cmd, show_output=True, show_cmd=True, docker=True):
     if docker:
@@ -44,7 +48,8 @@ def get_credentials():
 
     return json.dumps(data)
 
-
-#Login To hbdclient with user that has access
-#cursor.execute(f'UPSERT "XSA_KEY_VAULT"."XSA_KEY_VAULT.db.Tables::Key_Vault.Keys" VALUES ({project_name}, {get_credentials()}) WHERE KEY = {project_name}')
-
+#
+conn = dbapi.connect(address = hana_host, port = hana_port, user = xsa_keyuser, password = xsa_pass) 
+conn.cursor().execute(f"""
+UPSERT "XSA_KEY_VAULT"."XSA_KEY_VAULT.db.Tables::Key_Vault.Keys" VALUES ('{project_name}', '{get_credentials()}') WHERE APPNAME = '{project_name}'
+""") 

b8473e5ac035c7c1d98b7328436a1c013424c692
NIHO1009 <nichol@dsb.dk>
2021-10-27 09:44:41 +0200

    halloooooo

--- Infrastructure/keyvault.py
+++ Infrastructure/keyvault.py
@@ -30,10 +30,9 @@ def insert_key(project_name,hana_host,xsa_keyuser,xsa_pass):
     check_output(f'xs env {project_name} --export-json env.json')
     env_json = check_output(f'cat env.json')
 
-
     data = json.loads(env_json)
     data = {key: value for key, value in data['VCAP_SERVICES']['xsuaa'][0]['credentials'].items() if key in ['clientid', 'clientsecret', 'url']}
-   
+    data = json.dumps(data)
     conn = dbapi.connect(address = hana_host, port = hana_port, user = xsa_keyuser, password = xsa_pass) 
     conn.cursor().execute(f"""
         UPSERT "XSA_KEY_VAULT"."XSA_KEY_VAULT.db.Tables::Key_Vault.Keys" VALUES ('{project_name}', '{data}') WHERE APPNAME = '{project_name}'

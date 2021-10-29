b7c415ce60b7e94fa2ad450b107052c35adf62c1
milimat0810 <milimat@dsb.dk>
2021-10-29 09:30:53 +0200

    Test

--- Infrastructure/afload.py
+++ Infrastructure/afload.py
@@ -1,60 +1,60 @@
-import subprocess, sys
-from pathlib import Path
+# import subprocess, sys
+# from pathlib import Path
 
-def check_output(cmd, show_output=True, show_cmd=True):
-    if show_cmd:
-        print('Executing command: ')
-        print(cmd)
-    popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    output = ''
-    while popen.poll() is None:
-        line = popen.stdout.readline()
-        output += line
-        if show_output:
-            print(line)
-    return output
+# def check_output(cmd, show_output=True, show_cmd=True):
+#     if show_cmd:
+#         print('Executing command: ')
+#         print(cmd)
+#     popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+#     output = ''
+#     while popen.poll() is None:
+#         line = popen.stdout.readline()
+#         output += line
+#         if show_output:
+#             print(line)
+#     return output
 
-environment = get_octopusvariable("Octopus.Environment.Name").lower()
+# environment = get_octopusvariable("Octopus.Environment.Name").lower()
 
-if environment == 'sit':
-    sys.exit(1)
+# if environment == 'sit':
+#     sys.exit(1)
 
-print("*******************************************************************")
-print(" START afload.ps1")
-print("*******************************************************************")
+# print("*******************************************************************")
+# print(" START afload.ps1")
+# print("*******************************************************************")
 
-###############################################################################
-# Get all relevant parameters from octopus (variable set dataART)
-###############################################################################
+# ###############################################################################
+# # Get all relevant parameters from octopus (variable set dataART)
+# ###############################################################################
 
-project_name = get_octopusvariable("Octopus.Project.Name")
-release_number = get_octopusvariable("Octopus.Release.Number")
-container_name = f"dataArt.{project_name}.{release_number}.{environment}"
+# project_name = get_octopusvariable("Octopus.Project.Name")
+# release_number = get_octopusvariable("Octopus.Release.Number")
+# container_name = f"dataArt.{project_name}.{release_number}.{environment}"
 
-artifactory_login = get_octopusvariable("artifactory.login")
-artifactory_registry = get_octopusvariable("artifactory.registry")
-artifactory_pass = sys.argv[1]
+# artifactory_login = get_octopusvariable("artifactory.login")
+# artifactory_registry = get_octopusvariable("artifactory.registry")
+# artifactory_pass = sys.argv[1]
 
-###############################################################################
-# Stop and delete containers
-###############################################################################
+# ###############################################################################
+# # Stop and delete containers
+# ###############################################################################
 
-print(container_name)
+# print(container_name)
 
-check_output(f'docker container stop {container_name}')
-check_output('docker container prune -f')
+# check_output(f'docker container stop {container_name}')
+# check_output('docker container prune -f')
 
-###############################################################################
-# Login to artifactory, pull and start XSA__AP_CLI_DEPLOY container
-###############################################################################
+# ###############################################################################
+# # Login to artifactory, pull and start XSA__AP_CLI_DEPLOY container
+# ###############################################################################
 
-pwd = Path.cwd().parent.parent
+# pwd = Path.cwd().parent.parent
 
-check_output(f'docker login -u {artifactory_login} -p {artifactory_pass} {artifactory_registry}')
+# check_output(f'docker login -u {artifactory_login} -p {artifactory_pass} {artifactory_registry}')
 
-check_output('docker pull artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
-check_output(f'docker run -v {pwd}:/data --name {container_name} --rm -t -d artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
+# check_output('docker pull artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
+# check_output(f'docker run -v {pwd}:/data --name {container_name} --rm -t -d artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
 
-print("*******************************************************************")
-print(" STOP afload.ps1")
-print("*******************************************************************")
+# print("*******************************************************************")
+# print(" STOP afload.ps1")
+# print("*******************************************************************")
--- Infrastructure/ap_deploy.py
+++ Infrastructure/ap_deploy.py
@@ -33,6 +33,30 @@ def check_output(cmd, show_output=True, show_cmd=True, docker=True):
             print(line)
     return output
 
+artifactory_login = get_octopusvariable("artifactory.login")
+artifactory_registry = get_octopusvariable("artifactory.registry")
+artifactory_pass = sys.argv[1]
+
+###############################################################################
+# Stop and delete containers
+###############################################################################
+
+print(container_name)
+
+check_output(f'docker container stop {container_name}')
+check_output('docker container prune -f')
+
+###############################################################################
+# Login to artifactory, pull and start XSA__AP_CLI_DEPLOY container
+###############################################################################
+
+pwd = Path.cwd().parent.parent
+
+check_output(f'docker login -u {artifactory_login} -p {artifactory_pass} {artifactory_registry}')
+
+check_output('docker pull artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
+check_output(f'docker run -v {pwd}:/data --name {container_name} --rm -t -d artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
+
 
 with open('../../manifest.yml') as manifest:
     manifest_yaml = manifest.read()

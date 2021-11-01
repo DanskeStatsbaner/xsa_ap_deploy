76449e348bd6c82ae10f499d8f882b433c5876ad
NIHO1009 <nichol@dsb.dk>
2021-11-01 12:28:42 +0100

    tester

--- Infrastructure/test.py
+++ Infrastructure/test.py
@@ -120,11 +120,11 @@ if is_web:
         
 
         for role_collection in role_collections:  
-            user = role_collection   
+            user = role_collection + 'dev'   
             #password = get_random_password()
             password = 'Pass1234'                            
-            check_output(f'xs create-user {user} {password} -p {xsa_pass} --no-password-change',show_output=True, show_cmd=False)   
-            check_output(f'xs assign-role-collection {role_collection} {user} -u {xsa_user} -p {xsa_pass}' ,show_output=True, show_cmd=False)
+            check_output(f'xs create-user {user} {password} -p {xsa_pass} --no-password-change',show_output=True, show_cmd=True)   
+            check_output(f'xs assign-role-collection {role_collection} {user} -u {xsa_user} -p {xsa_pass}' ,show_output=True, show_cmd=True)
             #check_output(f'xs assign-role-collection XS_CONTROLLER_USER {user} -u {xsa_user} -p {xsa_pass}' ,show_output=True, show_cmd=False)
             template += f"""
             Username: {role_collection}

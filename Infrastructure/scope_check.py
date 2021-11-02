36ac81792e825bc1c6c4acd6ce5cc0aea3c26307
milimat0810 <milimat@dsb.dk>
2021-11-02 10:43:15 +0100

    Change scope

--- Infrastructure/scope_check.py
+++ Infrastructure/scope_check.py
@@ -7,7 +7,7 @@ scope = APIRouter()
 
 # Using Request instance
 @scope.get("/url-list-from-request")
-async def get_all_urls_from_request(request: Request, security_context=Depends(auth(scope='uaa.resource'))):
+async def get_all_urls_from_request(request: Request, security_context=Depends(auth(scope='ADMIN'))):
     url_list = [
         {"path": route.path, "name": route.name} for route in request.scope.routes
     ]

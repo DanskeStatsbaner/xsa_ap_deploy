aac94fdfb4040ac1adf15e51eaa2d9f0699461c1
milimat0810 <milimat@dsb.dk>
2021-11-02 12:17:19 +0100

    Fix

--- Infrastructure/scope_check.py
+++ Infrastructure/scope_check.py
@@ -9,7 +9,8 @@ scope = APIRouter()
 @scope.get("/url-list-from-request")
 async def get_all_urls_from_request(request: Request, security_context=Depends(auth(scope='ADMIN'))):
     url_list = [
-        {"path": route.path, "name": route.name} for route in request.scope.routes
+        {'path': route.path, 'name': route.name}
+        for route in request.app.routes
     ]
     return url_list
 

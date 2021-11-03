c3018a8f9fb557aeb843a2c90b8d942c3145baff
milimat0810 <milimat@dsb.dk>
2021-11-03 20:59:05 +0100

    Test

--- Infrastructure/ap_deploy.py
+++ Infrastructure/ap_deploy.py
@@ -252,11 +252,11 @@ with open('env.json') as env_json:
     url = data["url"]
 
 
-jwt = check_output(f"curl -X POST {url}/oauth/token -u \\'{clientid}:{clientsecret}\\' -d \\'grant_type=client_credentials&token_format=jwt\\'")
+jwt = check_output(f'curl -X POST {url}/oauth/token -u "{clientid}:{clientsecret}" -d "grant_type=client_credentials&token_format=jwt"', docker=False)
 
-command = f"curl -X POST https://{host}.xsabi{hana_environment}.dsb.dk:30033/scope-check -H \\'accept: application/json\\' -H \\'Authorization: Bearer {jwt}\\'"
+command = f'curl -X POST https://{host}.xsabi{hana_environment}.dsb.dk:30033/scope-check -H "accept: application/json" -H "Authorization: Bearer {jwt}"'
 
-output = check_output(command)
+output = check_output(command, docker=False)
 
 printhighlight(output)
 set_octopusvariable("Scopes", 'Test')

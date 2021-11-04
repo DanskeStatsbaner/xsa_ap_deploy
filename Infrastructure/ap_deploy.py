edd1f6c5d31c617e956160ec65a717ad95a40367
NIHO1009 <nichol@dsb.dk>
2021-11-04 11:37:37 +0100

    test

--- Infrastructure/ap_deploy.py
+++ Infrastructure/ap_deploy.py
@@ -251,22 +251,23 @@ with open('env.json') as env_json:
     clientsecret = data["clientsecret"]
     url = data["url"]
 
+try: 
+    credentials = check_output(f'curl -s -X POST {url}/oauth/token -u "{clientid}:{clientsecret}" -d "grant_type=client_credentials&token_format=jwt"', show_output=False, show_cmd=False, docker=False)
 
-credentials = check_output(f'curl -s -X POST {url}/oauth/token -u "{clientid}:{clientsecret}" -d "grant_type=client_credentials&token_format=jwt"', show_output=False, show_cmd=False, docker=False)
+    jwt = json.loads(credentials)['access_token']
 
-jwt = json.loads(credentials)['access_token']
+    output = check_output(f'curl -s -X GET https://{host}.xsabi{hana_environment}.dsb.dk:30033/scope-check -H "accept: application/json" -H "Authorization: Bearer {jwt}"', show_cmd=False, docker=False)
+    output = json.loads(output)
 
-output = check_output(f'curl -s -X GET https://{host}.xsabi{hana_environment}.dsb.dk:30033/scope-check -H "accept: application/json" -H "Authorization: Bearer {jwt}"', show_cmd=False, docker=False)
-set_octopusvariable("Test", output)
-output = json.loads(output)
+    template = ''
+    margin = max([len(endpoint) for title, endpoints in output.items() for endpoint, scope in endpoints.items()]) + 10
+    for title, endpoints in output.items():
+        template += f"\n{title}\n{'Endpoint:':{margin}}Scope:\n"
+        for endpoint, scope in endpoints.items():
+            template += f"{endpoint:{margin}}{scope}\n"        
+   
+    set_octopusvariable("Scopes", template.strip())
+except Exception as ex:
+    failstep(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
 
-template = ''
-margin = max([len(endpoint) for title, endpoints in output.items() for endpoint, scope in endpoints.items()]) + 10
-for title, endpoints in output.items():
-    template += f"\n{title}\n{'Endpoint:':{margin}}Scope:\n"
-    for endpoint, scope in endpoints.items():
-        template += f"{endpoint:{margin}}{scope}\n"
-    
-set_octopusvariable("Scopes", template.strip())
-set_octopusvariable("Hardcode", 'Hardcode')
 

9b87236cca28c6335223dcdd2e7ce622d6173804
milimat0810 <milimat@dsb.dk>
2021-11-05 14:41:07 +0100

    Strip and refactor

--- Infrastructure/ap_deploy.py
+++ Infrastructure/ap_deploy.py
@@ -124,21 +124,15 @@ environment_variables = {
 }
 
 for variable, value in environment_variables.items():
-    paths = check_output(f"cd /data/app && grep -rwl -e '{variable}'").split('\n')
+    paths = check_output(f"cd /data/app && grep -rwl -e '{variable}'").strip().split('\n')
     
     for path in paths:
-        printhighlight('../../app/' + path)
-        printhighlight(os.path.exists('../../app/' + path))
+        with open('../../app/' + path) as file:
+            content = file.read()
+            content = content.replace(variable, value)
 
-        try:
-            with open('../../app/' + path) as file:
-                content = file.read()
-                content = content.replace(variable, value)
-
-            with open('../../app/' + path, 'w') as file:
-                file.write(content)
-        except:
-            pass
+        with open('../../app/' + path, 'w') as file:
+            file.write(content)
 
 # Web Section Starts
 if is_web:

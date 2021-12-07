9067359eaca49fa6bc6da482863d0079c264a329
milimat0810 <milimat@dsb.dk>
2021-12-07 12:43:42 +0100

    Refactor

--- Infrastructure/install.py
+++ Infrastructure/install.py
@@ -1,15 +1,6 @@
-import os
-import subprocess
-import json
-import yaml
-import sys
-import traceback
+import os, json, yaml, sys, traceback
 from pathlib import Path
-
-octopus_variables = ["Octopus.Environment.Name", "Octopus.Release.Number", "dataART.HumioIngestToken", "dataART.XSAUrl", "dataART.XSAUser",
-                     "dataART.XSASpace", "dataART.XSAKeyUser", "dataART.Host", "dataART.Database", "artifactory.login", "artifactory.registry"]
-octopus = {variable: get_octopusvariable(
-    variable) for variable in octopus_variables}
+from deploy_helper import check_output
 
 environment = get_octopusvariable("Octopus.Environment.Name").lower()
 
@@ -23,9 +14,6 @@ xsa_user = get_octopusvariable("dataART.XSAUser")
 xsa_space = get_octopusvariable("dataART.XSASpace")
 xsa_pass = sys.argv[1]
 
-xsa_keyuser = get_octopusvariable("dataART.XSAKeyUser")
-hana_host = get_octopusvariable("dataART.Host").split('.')[0]
-
 hana_environment = get_octopusvariable("dataART.Database").lower()
 hana_environment_upper = hana_environment.upper()
 
@@ -37,59 +25,32 @@ is_web = os.path.exists('../../xs-security.json')
 
 set_octopusvariable("Web", str(is_web))
 
-
-def check_output(cmd, show_output=True, show_cmd=True, docker=True):
-    if docker:
-        cmd = f'docker exec -it {container_name} /bin/sh -c "{cmd}"'
-    if show_cmd:
-        print('Executing command: ')
-        print(cmd)
-    popen = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT, universal_newlines=True)
-    output = ''
-    while popen.poll() is None:
-        line = popen.stdout.readline()
-        output += line
-        if show_output:
-            print(line, end='')
-    return output
+#shell = lambda cmd, show_output=True, show_cmd=True, docker=True: check_output(f'docker exec -it {container_name} /bin/sh -c "{cmd}"' if docker else cmd, show_output, show_cmd)
 
 ###############################################################################
 # Stop and delete containers
 ###############################################################################
 
-
 print(container_name)
 
-#docker_client = docker.from_env()
-
-check_output(f'docker container stop {container_name}', docker=False)
-# docker_client.containers.get(container_name).stop()
-
-check_output('docker container prune -f', docker=False)
-# docker_client.containers.prune()
+shell(f'docker container stop {container_name}', docker=False)
+shell('docker container prune -f', docker=False)
 
 ###############################################################################
 # Login to artifactory, pull and start XSA__AP_CLI_DEPLOY container
 ###############################################################################
+
 pwd = Path.cwd().parent.parent
 
-#docker_client.login(username=artifactory_login, password=artifactory_pass, registry=artifactory_registry)
-check_output(
-    f'docker login -u {artifactory_login} -p {artifactory_pass} {artifactory_registry}', show_cmd=False, docker=False)
+shell(f'docker login -u {artifactory_login} -p {artifactory_pass} {artifactory_registry}', show_cmd=False, docker=False)
 
-# docker_client.images.pull('artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy')
-check_output(
-    'docker pull artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy', docker=False)
-
-#docker_client.containers.run('artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy', name=container_name, volumes=[f'{pwd}:/data'], auto_remove=True, tty=True,  detach=True)
-check_output(
-    f'docker run -v {pwd}:/data --name {container_name} --rm -t -d artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy', docker=False)
+shell('docker pull artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy', docker=False)
+shell(f'docker run -v {pwd}:/data --name {container_name} --rm -t -d artifactory.azure.dsb.dk/docker/xsa_ap_cli_deploy', docker=False)
 
 
 with open('../../manifest.yml') as manifest:
     manifest_yaml = manifest.read()
-
+    
 manifest_dict = yaml.safe_load(manifest_yaml)
 
 project_type = manifest_dict['type']
@@ -103,11 +64,7 @@ host = project_name.lower().replace('_', '-')
 app_router = f'{project_name}-sso'
 app_router_host = app_router.lower().replace('_', '-')
 uaa_service = f'{project_name}-uaa'
-
-
-def url(
-    subdomain): return f"https://{subdomain}.xsabi{hana_environment}.dsb.dk:30033"
-
+url = lambda subdomain: f"https://{subdomain}.xsabi{hana_environment}.dsb.dk:30033"
 
 services += [uaa_service]
 
@@ -120,24 +77,25 @@ manifest_dict = {
             'command': 'python api.py',
             'services': services
         }
-    ]
-}
-# IF WEBAPP, appended app_router part to manifest_dicti
+        ]
+    }
+        # IF WEBAPP, appended app_router part to manifest_dicti
 
-app_router_dict = {
-    'name': app_router,
-    'host': app_router_host,
-    'path': './app-router/',
+app_router_dict =  {
+            'name': app_router,
+            'host': app_router_host,
+            'path': './app-router/',
             'env': {
                 'destinations': json.dumps([{"name": project_name, "url": url(host), "forwardAuthToken": True}])
             },
-    'services': [
+            'services': [
                 uaa_service
             ]
-}
+        }
 
 if is_web:
-    manifest_dict['applications'] += [app_router_dict]
+    manifest_dict['applications'] += [app_router_dict]     
+    
 
 
 manifest_yaml = yaml.dump(manifest_dict)
@@ -147,21 +105,19 @@ with open('../../manifest.yml', 'w') as file:
 
 with open('../../app/manifest', 'w') as file:
     file.write(manifest_yaml)
-
+    
 environment_variables = {
     'OCTOPUS_APP_ROUTER_URL': url(app_router_host),
     'OCTOPUS_HUMIO_INGEST_TOKEN': humio_ingest_token,
     'OCTOPUS_PROJECT_NAME': project_name,
-    'OCTOPUS_RELEASE_NUMBER': release_number,
-    'OCTOPUS_APP_URL': url(host)
+    'OCTOPUS_RELEASE_NUMBER': release_number
 }
 
 for variable, value in environment_variables.items():
-    paths = check_output(
-        f"cd /data/app && grep -rwl -e '{variable}'").strip().split('\n')
-
+    paths = shell(f"cd /data/app && grep -rwl -e '{variable}'").strip().split('\n')
+    
     paths = [path for path in paths if path != '']
-
+    
     for path in paths:
         with open('../../app/' + path, encoding="utf-8") as file:
             content = file.read()
@@ -193,10 +149,10 @@ if is_web:
     with open('../../xs-security.json') as file:
         xs_security = json.loads(file.read())
         xs_security['xsappname'] = project_name
-
+        
         for index, scope in enumerate(xs_security['scopes']):
             xs_security['scopes'][index]['name'] = f'$XSAPPNAME.{scope["name"]}'
-
+            
         scopes = [scope['name'] for scope in xs_security['scopes']]
         role_collections = []
         mappings = []
@@ -204,12 +160,10 @@ if is_web:
         for index, role in enumerate(xs_security['role-templates']):
             role_collection = f'{project_name}_{role["name"]}'
             role_collections += [role_collection]
-            mappings += [[role_collection, f'SHIP.{hana_environment_upper}.{scope}']
-                         for scope in role['scope-references']]
+            mappings += [[role_collection, f'SHIP.{hana_environment_upper}.{scope}'] for scope in role['scope-references']]
             xs_security['role-templates'][index]['name'] = f'{project_name}_{role["name"]}'
-            xs_security['role-templates'][index]['scope-references'] = [
-                f'$XSAPPNAME.{scope}' for scope in role['scope-references']]
-
+            xs_security['role-templates'][index]['scope-references'] = [f'$XSAPPNAME.{scope}' for scope in role['scope-references']]
+            
         roles = [role['name'] for role in xs_security['role-templates']]
 
         xs_security = json.dumps(xs_security, indent=2)
@@ -217,56 +171,45 @@ if is_web:
     with open('../../xs-security.json', 'w') as file:
         file.write(xs_security)
 # Web Ends
-
-
 def delete_manifest():
     if os.path.exists('app/manifest'):
         os.remove('app/manifest')
 
+shell(f'xs login -u {xsa_user} -p {xsa_pass} -a {xsa_url} -o orgname -s {xsa_space}', show_cmd=False)
 
-check_output(
-    f'xs login -u {xsa_user} -p {xsa_pass} -a {xsa_url} -o orgname -s {xsa_space}', show_cmd=False)
-
-output = check_output(f'xs service {uaa_service}', show_output=True).lower()
+output = shell(f'xs service {uaa_service}', show_output=True).lower()
 #printhighlight('output 1' + output)
 
-xs_security = '-c xs-security.json' if os.path.exists(
-    '../../xs-security.json') else ''
+xs_security = '-c xs-security.json' if os.path.exists('../../xs-security.json') else ''
 
 if 'failed' in output:
-    failstep(
-        f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
+    failstep(f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
 elif not 'succeeded' in output:
-    output = check_output(
-        f'cd /data && xs create-service xsuaa default {uaa_service} {xs_security}', show_output=True)
+    output = shell(f'cd /data && xs create-service xsuaa default {uaa_service} {xs_security}', show_output=True)
     if 'failed' in output:
-        failstep(f'Creation of the service "{uaa_service}" failed' + '\n'.join(
-            [line for line in output.split('\n') if 'FAILED' in line]))
-    else:
-        #printhighlight('output 3' + output)
+        failstep(f'Creation of the service "{uaa_service}" failed' + '\n'.join([line for line in output.split('\n') if 'FAILED' in line]))
+    else:  
+        #printhighlight('output 3' + output) 
         printhighlight(f'The service "{uaa_service}" was succesfully created')
 else:
-    output = check_output(
-        f'cd /data && xs update-service {uaa_service} {xs_security}', show_output=True)
+    output = shell(f'cd /data && xs update-service {uaa_service} {xs_security}', show_output=True)
     #printhighlight('output 4' + output)
 
     if 'failed' in output:
-        failstep(
-            f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
+        failstep(f'The service "{uaa_service}" is broken. Try to delete the service with: "xs delete-service {uaa_service}" and rerun xs_push.py.')
 
 # Web Starts
-if is_web:
-    app_router_output = check_output(f'cd /data && xs push {app_router}')
+if is_web:       
+    app_router_output = shell(f'cd /data && xs push {app_router}')
     #printhighlight('app_router_output :' + app_router_output)
 # Web Ends
 
-app_output = check_output(f'cd /data && xs push {project_name}')
+app_output = shell(f'cd /data && xs push {project_name}')
 #printhighlight('app output: ' +app_output)
-output = app_router_output if is_web else app_output
+output = app_router_output if is_web else app_output 
 
 
-app_url = [line.split(':', 1)[1].strip() for line in output.split(
-    '\n') if 'urls' in line][0] + '/' + host
+app_url = [line.split(':', 1)[1].strip() for line in output.split('\n') if 'urls' in line][0] + '/' + host
 
 is_running = app_output.rfind('RUNNING') > app_output.rfind('CRASHED')
 
@@ -275,50 +218,41 @@ if is_running:
 else:
     failstep('The application crashed')
 
-check_output(
-    f'cd /data/Deployment/Scripts && xs env {project_name} --export-json env.json', show_output=True, show_cmd=True)
+shell(f'cd /data/Deployment/Scripts && xs env {project_name} --export-json env.json', show_output=True, show_cmd=True)
 
 # Web Starts
 if is_web:
     for role_collection in role_collections:
-        check_output(
-            f'xs delete-role-collection {role_collection} -f -u {xsa_user} -p {xsa_pass}', show_cmd=False)
-        check_output(
-            f'xs create-role-collection {role_collection} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
-        check_output(
-            f'xs update-role-collection {role_collection} --add-role {role_collection} -s {xsa_space} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
+        shell(f'xs delete-role-collection {role_collection} -f -u {xsa_user} -p {xsa_pass}', show_cmd=False)
+        shell(f'xs create-role-collection {role_collection} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
+        shell(f'xs update-role-collection {role_collection} --add-role {role_collection} -s {xsa_space} -u {xsa_user} -p {xsa_pass}', show_cmd=False)
     try:
         mappings = json.dumps(mappings).replace('"', '\\"')
-        check_output(
-            f"cd /data/Deployment/Scripts && python3 cockpit.py -u {xsa_user} -p {xsa_pass} -a {xsa_url} -m '{mappings}'", show_cmd=False)
+        shell(f"cd /data/Deployment/Scripts && python3 cockpit.py -u {xsa_user} -p {xsa_pass} -a {xsa_url} -m '{mappings}'", show_cmd=False)
     except Exception as ex:
-        failstep(''.join(traceback.format_exception(
-            etype=type(ex), value=ex, tb=ex.__traceback__)))
+        failstep(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
 # Web Ends
 
 try:
-    check_output(
-        f"cd /data/Deployment/Scripts && python3 keyvault.py -n {project_name} -h {hana_host} -u {xsa_keyuser} -p {xsa_pass}", show_cmd=False)
+    xsa_keyuser = get_octopusvariable("dataART.XSAKeyUser")
+    hana_host = get_octopusvariable("dataART.Host").split('.')[0]
+    shell(f"cd /data/Deployment/Scripts && python3 keyvault.py -n {project_name} -h {hana_host} -u {xsa_keyuser} -p {xsa_pass}", show_cmd=False)
 except Exception as ex:
-    failstep(''.join(traceback.format_exception(
-        etype=type(ex), value=ex, tb=ex.__traceback__)))
+    failstep(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
 
 with open('env.json') as env_json:
     data = json.load(env_json)
-    data = {key: value for key, value in data['VCAP_SERVICES']['xsuaa'][0]['credentials'].items(
-    ) if key in ['clientid', 'clientsecret', 'url']}
+    data = {key: value for key, value in data['VCAP_SERVICES']['xsuaa'][0]['credentials'].items() if key in ['clientid', 'clientsecret', 'url']}
     clientid = data["clientid"]
     clientsecret = data["clientsecret"]
     url = data["url"]
 
 
-credentials = check_output(
-    f'curl -s -X POST {url}/oauth/token -u "{clientid}:{clientsecret}" -d "grant_type=client_credentials&token_format=jwt"', show_output=False, show_cmd=False, docker=False)
+credentials = check_output(f'curl -s -X POST {url}/oauth/token -u "{clientid}:{clientsecret}" -d "grant_type=client_credentials&token_format=jwt"', show_output=True, show_cmd=True)
 
 jwt = json.loads(credentials)['access_token']
 
-output = check_output(
-    f'curl -s -X GET https://{host}.xsabi{hana_environment}.dsb.dk:30033/scope-check -H "accept: application/json" -H "Authorization: Bearer {jwt}"', show_cmd=False, docker=False)
+output = shell(f'curl -s -X GET https://{host}.xsabi{hana_environment}.dsb.dk:30033/scope-check -H "accept: application/json" -H "Authorization: Bearer {jwt}"', show_cmd=False, docker=False)
 set_octopusvariable("Workaround", 'Workaround')
 output = json.loads(output)
 
@@ -333,8 +267,7 @@ predefined_endpoints = [
 
 template = ''
 for title, endpoints in output.items():
-    endpoints = {endpoint: scope for endpoint, scope in endpoints.items(
-    ) if endpoint not in predefined_endpoints}
+    endpoints = {endpoint: scope for endpoint, scope in endpoints.items() if endpoint not in predefined_endpoints}
     if len(endpoints) > 0:
         template += f'<h3>{title}</h3>'
         template += f'<table>'

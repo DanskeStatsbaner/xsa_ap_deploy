f35c4615ff468bf4aad1cd8def7a7cdfb1e0f162
milimat0810 <milimat@dsb.dk>
2021-12-16 12:01:06 +0100

    Testing

--- Infrastructure/octopus/deploy_helper.py
+++ Infrastructure/octopus/deploy_helper.py
@@ -1,4 +1,4 @@
-import subprocess, random, string, os, sys, textwrap, logging, logging.config
+import subprocess, random, string, os, sys, logging, logging.config
 
 class _ExcludeErrorsFilter(logging.Filter):
     def filter(self, record):
@@ -46,15 +46,6 @@ config = {
 logging.config.dictConfig(config)
 
 print = logging.info
-
-def banner(title, width=70, padding=2):
-    lines = []
-    for line in title.split('\n'):
-        lines += textwrap.wrap(line, width=width - padding)
-    centered_lines = [f'{line:^{width}}' for line in lines]
-    seperator = ['#' * (width)]
-    for line in seperator + centered_lines + seperator:
-        print(line)
 
 def run(cmd, env={}, pipe=None, worker=None, show_output=True, show_cmd=True, ignore_errors=False, exception_handler=None):
     if pipe is not None and pipe in env:

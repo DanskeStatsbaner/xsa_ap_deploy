6bb2daccf29ade2c5b5a62a1234edf727b0cf4a2
milimat0810 <milimat@dsb.dk>
2021-12-17 09:06:02 +0100

    Testing

--- Infrastructure/octopus/deploy_helper.py
+++ Infrastructure/octopus/deploy_helper.py
@@ -53,11 +53,9 @@ def banner(title, width=70, padding=2):
         lines += textwrap.wrap(line, width=width - padding)
     centered_lines = [f'{line:^{width}}' for line in lines]
     seperator = '#' * (width)
-    print(f"""
-{seperator}
-{os.linesep.join(centered_lines)}
-{seperator}
-        """)
+    print(seperator)
+    print(os.linesep.join(centered_lines))
+    print(seperator)
 
 def run(cmd, env={}, pipe=None, worker=None, show_output=True, show_cmd=True, ignore_errors=False, exception_handler=None):
     if pipe is not None and pipe in env:

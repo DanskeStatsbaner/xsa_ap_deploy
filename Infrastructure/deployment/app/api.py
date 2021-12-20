19b7252cf8dad5fd89ee9fd9645f174d9f3c4423
milimat0810 <milimat@dsb.dk>
2021-12-20 17:01:47 +0100

    Testing

--- Infrastructure/deployment/app/api.py
+++ Infrastructure/deployment/app/api.py
@@ -151,8 +151,8 @@ async def upload(path: str = '', file: UploadFile=File(...), security_context=De
 
     return {"Result": "OK"}
 
-@router.get("/scope-check")
-async def scope_check(request: Request, security_context=Depends(auth(scope='THE_FORM'))):
+@router.get("/scope-check2")
+async def scope_check(request: Request, security_context=Depends(auth(scope='uaa.resource'))):
     endpoints = [route for route in request.app.routes if type(route) == APIRoute]
     websockets = [route for route in request.app.routes if type(route) == APIWebSocketRoute]
 

3d43787edca50ea8a339ba9718cb616ede7260b8
milimat0810 <milimat@dsb.dk>
2021-12-20 09:01:30 +0100

    Testing

--- Infrastructure/octopus/deploy_helper.py
+++ Infrastructure/octopus/deploy_helper.py
@@ -1,53 +1,6 @@
-import subprocess, random, string, os, sys, textwrap, logging, logging.config
+import subprocess, random, string, os, sys, textwrap
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
-
-class _ExcludeErrorsFilter(logging.Filter):
-    def filter(self, record):
-        """Only returns log messages with log level below ERROR (numeric value: 40)."""
-        return record.levelno < 40
-
-config = {
-    'version': 1,
-    'filters': {
-        'exclude_errors': {
-            '()': _ExcludeErrorsFilter
-        }
-    },
-    'formatters': {
-        # Modify log message format here or replace with your custom formatter class
-        'my_formatter': {
-            'format': '%(message)s'
-        }
-    },
-    'handlers': {
-        'console_stderr': {
-            # Sends log messages with log level ERROR or higher to stderr
-            'class': 'logging.StreamHandler',
-            'level': 'ERROR',
-            'formatter': 'my_formatter',
-            'stream': sys.stderr
-        },
-        'console_stdout': {
-            # Sends log messages with log level lower than ERROR to stdout
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
-            'formatter': 'my_formatter',
-            'filters': ['exclude_errors'],
-            'stream': sys.stdout
-        }
-    },
-    'root': {
-        # In general, this should be kept at 'NOTSET'.
-        # Otherwise it would interfere with the log levels set for each handler.
-        'level': 'NOTSET',
-        'handlers': ['console_stderr', 'console_stdout']
-    },
-}
-
-logging.config.dictConfig(config)
-
-print = lambda message: logging.info(' ' + str(message))
 
 def banner(title, width=70, padding=2):
     lines = []
